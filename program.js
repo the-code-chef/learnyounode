60b7b0bc7dedb30a7c492a557fc32f50389fd7c3
Deepanshu Sinha <107sinha@gmail.com>
2017-02-13 18:32:56 +0530

    baby steps

--- program.js
+++ program.js
@@ -1,1 +1,21 @@
-console.log("HELLO WORLD")
+// My solution
+
+var numbers = process.argv.slice(2)
+
+var sum = numbers.reduce(
+	function (curr, prev) {
+		// body...
+		return +curr + +prev;
+	});
+
+console.log(sum)
+
+// Official Solution
+
+var result = 0
+
+for (var i = 2; i < process.argv.length; i++) {
+	result += Number(process.argv[i])
+}
+
+console.log(result)
